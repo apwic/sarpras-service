617b6c6d4f87cad7bc7138f1255a6c020c7822c0
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2023-03-14 21:00:43 +0700

    feat: get facility id

--- src/controllers/facility-controller.js
+++ src/controllers/facility-controller.js
@@ -3,6 +3,7 @@ const buildResponse = require('../utils/build-response');
 
 const uploadFile = require('../middlewares/file');
 const JWTMiddleware = require('../middlewares/jwt');
+const UserValidation = require('../middlewares/user-validation');
 
 const FacilityService = require('../services/facility-service');
 const facilityRouter = require('express').Router();
@@ -10,8 +11,14 @@ const facilityRouter = require('express').Router();
 module.exports = () => {
 	facilityRouter.post(
 		'/vehicle',
-		[JWTMiddleware.verifyToken, uploadFile],
+		[JWTMiddleware.verifyToken, UserValidation.admin, uploadFile],
 		handleRequest(async (req) => FacilityService.createFacility(req.body, req.files, req.user.id)),
+		buildResponse()
+	);
+
+	facilityRouter.get(
+		'/vehicle/:id',
+		handleRequest(async (req) => FacilityService.getFacility(req.params.id)),
 		buildResponse()
 	);
 
