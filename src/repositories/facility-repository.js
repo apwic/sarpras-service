32c9e39aa2a70065c9befb7e726dcb2fc4ef720c
I Gede Arya Raditya Parameswara <71829426+gedearyarp@users.noreply.github.com>
2023-03-14 22:52:52 +0700

    feat: update vehicle

--- src/repositories/facility-repository.js
+++ src/repositories/facility-repository.js
@@ -47,6 +47,27 @@ class FacilityRepository {
         }
     }
 
+    static async updateFacility(facility) {
+        try {
+            await models.Facility.update({
+                pic_id: facility.pic_id,
+                category: facility.category,
+                electricity: facility.electricity,
+                utility: facility.utility,
+                price: facility.price,
+                description: facility.description,
+            }, {
+                where: {
+                    id: facility.id,
+                },
+            });
+        } catch (err) {
+            throw new StandardError(500, 'DATABASE_ERROR', 'Error occured in database', err, {
+                facility,
+            });
+        }
+    }
+
     static async createVehicle(vehicle) {
         try {
             return await models.FacilityVehicle.create({
@@ -77,6 +98,29 @@ class FacilityRepository {
         } catch (err) {
             throw new StandardError(500, 'DATABASE_ERROR', 'Error occured in database', err, {
                 id,
+            });
+        }
+    }
+
+    static async updateVehicle(vehicle) {
+        try {
+            await models.FacilityVehicle.update({
+                campus_id: vehicle.campus_id,
+                name: vehicle.name,
+                type: vehicle.type,
+                sim_category: vehicle.sim_category,
+                license_number: vehicle.license_number,
+                vehicle_capacity: vehicle.vehicle_capacity,
+                image: vehicle.image,
+                status_maintenance: vehicle.status_maintenance,
+            }, {
+                where: {
+                    id: vehicle.id,
+                },
+            });
+        } catch (err) {
+            throw new StandardError(500, 'DATABASE_ERROR', 'Error occured in database', err, {
+                vehicle,
             });
         }
     }
--- src/services/facility-service/index.js
+++ src/services/facility-service/index.js
@@ -76,6 +76,62 @@ class FacilityService {
         };
     }
 
+    static async updateFacilityVehicle(id, data, files) {
+        const facility = await FacilityRepository.getFacility(id);
+
+        if (facility.category !== 'VEHICLE') {
+            return {
+                message: 'Facility is not a vehicle',
+            }
+        }
+
+        const vehicle = await FacilityRepository.getVehicle(id);
+        const images = vehicle.image || [];
+
+        const uploadedImages = [];
+        const newImages = files.image || [];
+
+        await Promise.all(
+            newImages.map(async (image) => {
+                const fileURL = await ImageFacility.upload(id, image);
+                uploadedImages.push(fileURL);
+            })
+        );
+
+        const vehicleData = {
+            id: id,
+            campus_id: data.campus_id || vehicle.campus_id,
+            name: data.name || vehicle.name,
+            type: data.type || vehicle.type,
+            sim_category: data.sim_category || vehicle.sim_category,
+            license_number: data.license_number || vehicle.license_number,
+            vehicle_capacity: data.vehicle_capacity || vehicle.vehicle_capacity,
+            image: uploadedImages,
+            status_maintenance: data.status_maintenance || vehicle.status_maintenance,
+        }
+        await FacilityRepository.updateVehicle(vehicleData);
+
+        const facilityData = {
+            id: id,
+            pic_id: data.pic_id || facility.pic_id,
+            category: 'VEHICLE',
+            electricity: data.electricity || facility.electricity,
+            utility: data.utility || facility.utility,
+            price: data.price || facility.price,
+            description: data.description || facility.description,
+        }
+        await FacilityRepository.updateFacility(facilityData);
+
+        await Promise.all(
+            images.map(async (image) => {
+                await ImageFacility.delete(image);
+            }
+        ));
+
+        return {
+            message: 'Facility Vehicle updated succesfully',
+        };
+    }
 }
 
 module.exports = FacilityService;
