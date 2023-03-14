1d09333f8e315e69b6bf2f8626b46453582b06a1
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-03-14 16:56:59 +0700

    feat: random file name on upload

--- src/utils/upload-file.js
+++ src/utils/upload-file.js
@@ -1,15 +1,16 @@
 const uploadPromise = require('../clients/google-cloud-storage/index');
 const path = require('path');
+const crypto = require('crypto');
 
 async function uploadImageUser(oldPath, file) {
   const filePath = `image/user/${path.parse(oldPath).name + path.parse(oldPath).ext}`
-  const fileName = `image/user/${file.originalname.replace(/ /g, '_')}`;
+  const fileName = `image/user/${crypto.randomBytes(20).toString('hex') + path.parse(file.originalname).ext}`;
   return await uploadPromise(filePath, file, fileName);
 }
 
 async function uploadFileBooking(id, oldPath, file) {
   const filePath = `document/booking-${id}/${path.parse(oldPath).name + path.parse(oldPath).ext}`
-  const fileName = `document/booking-${id}/${file.originalname.replace(/ /g, '_')}`;
+  const fileName = `document/booking-${id}/${crypto.randomBytes(20).toString('hex') + path.parse(file.originalname).ext}`;
   return await uploadPromise(filePath, file, fileName);
 }
 
