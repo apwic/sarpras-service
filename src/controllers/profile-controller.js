1b4520065e634d483d90033e861b74e299bab5de
Adiyansa Prasetya Wicaksana <adiyansawicaksana@gmail.com>
2023-03-14 15:06:32 +0700

    feat: create booking

--- src/controllers/profile-controller.js
+++ src/controllers/profile-controller.js
@@ -1,12 +1,9 @@
-const Joi = require('joi');
-const expressValidation = require('express-validation').validate;
-
 const handleRequest = require('../utils/handle-request');
 const buildResponse = require('../utils/build-response');
 
 const JWTMiddleware = require('../middlewares/jwt');
 const UserService = require('../services/user-service');
-const { uploadSingleFile } = require('../middlewares/file');
+const uploadFile = require('../middlewares/file');
 
 const profileRouter = require('express').Router();
 
@@ -20,9 +17,9 @@ module.exports = () => {
 
 	profileRouter.put(
 		'/edit',
-		[JWTMiddleware.verifyToken, uploadSingleFile],
+		[JWTMiddleware.verifyToken, uploadFile],
 		handleRequest(async (req) =>
-			UserService.updateUser(req.user.id, req.file, req.body.no_telp)
+			UserService.updateUser(req.user.id, req.files.image[0], req.body.no_telp)
 		),
 		buildResponse()
 	);
--- src/middlewares/file/index.js
+++ src/middlewares/file/index.js
@@ -1,30 +1,68 @@
 const util = require('util');
 const multer = require('multer');
 const maxSize = 64 * 1024 * 1024;
-const directoryPath = '/../public/uploads';
 
-let storage = multer.memoryStorage({
-	destination: (req, file, cb) => {
-		cb(null, __basedir + directoryPath);
-	},
-	filename: (req, file, cb) => {
-		cb(null, file.originalname);
-	},
-});
-
-let uploadSingle = multer({
-	storage: storage,
-	limits: { fileSize: maxSize },
-}).single('file');
-
-let uploadMultiple = multer({
-	storage: storage,
-	limits: { fileSize: maxSize },
-}).array('file', 10);
-
-let uploadSingleFile = util.promisify(uploadSingle);
-let uploadMultipleFile = util.promisify(uploadMultiple);
-module.exports = {
-	uploadSingleFile,
-	uploadMultipleFile,
+const fileFilter = (req, file, cb) => {
+	if (file.fieldname === 'image') {
+		if (
+			file.mimetype === 'image/png' ||
+			file.mimetype === 'image/jpg' ||
+			file.mimetype === 'image/jpeg'
+		) {
+			// check file type to be png, jpeg, or jpg
+			cb(null, true);
+		} else {
+			cb(null, false); // else fails
+		}
+	} else if (file.fieldname === 'video') {
+		if (
+			file.mimetype === 'video/3gpp' ||
+			file.mimetype === 'video/quicktime' ||
+			file.mimetype === 'video/x-msvideo' ||
+			file.mimetype === '	video/x-ms-wmv'
+		) {
+			// check file type to be png, jpeg, or jpg
+			cb(null, true);
+		} else {
+			cb(null, false); // else fails
+		}
+	} else if (file.fieldname === 'file') {
+		if (
+			file.mimetype === 'application/pdf' ||
+			file.mimetype === 'application/msword' ||
+			file.mimetype ===
+				'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+		) {
+			// check file type to be pdf, doc, or docx
+			cb(null, true);
+		} else {
+			cb(null, false); // else fails
+		}
+	} else {
+		cb(null, false);
+	}
 };
+
+let uploadFileMulter = multer({
+	// storage: fileStorage,
+	limits: {
+		fileSize: maxSize,
+	},
+	fileFilter: fileFilter,
+}).fields([
+	{
+		name: 'image',
+		maxCount: 10,
+	},
+	{
+		name: 'video',
+		maxCount: 10,
+	},
+	{
+		name: 'file',
+		maxCount: 10,
+	},
+]);
+
+const uploadFile = util.promisify(uploadFileMulter);
+module.exports = uploadFile;
